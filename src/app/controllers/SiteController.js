2bb8d38b141643f454154da8c5376d833f2ae53f
Vinhlam2512 <lamvinh2431@gmail.com>
2021-09-02 00:51:26 +0700

    Refactor code: create route, app/controller folders

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,16 @@
+
+
+class SiteController {
+
+    // [GET] / 
+    index(req, res){
+        res.render('home');
+    }
+
+    // [GET] /search
+    search(req, res){
+        res.render('search'); // render trong views
+    }
+}
+
+module.exports = new SiteController;
